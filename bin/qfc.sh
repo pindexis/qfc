fd5d6e923b2db0f888c1c9470e9aa059baf49a3b
Integralist <mark.mcdx@gmail.com>
2017-08-10 08:41:35 +0100

    Fixes Issue #30

--- bin/qfc.sh
+++ bin/qfc.sh
@@ -9,7 +9,7 @@ function get_cursor_position(){
   # on my system, the following line can be replaced by the line below it
   echo -en "\033[6n" > /dev/tty
   # tput u7 > /dev/tty    # when TERM=xterm (and relatives)
-  IFS=';' read -r -d R  row col 
+  IFS=';' read -r -d R  row col
   stty $oldstty
   # change from one-based to zero based so they work with: tput cup $row $col
   row=$((${row:2} - 1))    # strip off the esc-[
@@ -46,7 +46,7 @@ if [[ -n "$ZSH_VERSION" ]]; then
         </dev/tty qfc --search="$word" --stdout="$tmp_file"
         result=$(<$tmp_file)
         rm -f $tmp_file
-        
+
         # append the completion path to the user buffer
         word_length=${#word}
         result_length=${#result}
@@ -59,14 +59,14 @@ if [[ -n "$ZSH_VERSION" ]]; then
         CURSOR=${offset}
     }
 
-    zle -N qfc_complete 
-    bindkey "$complete_shortcut" qfc_complete 
+    zle -N qfc_complete
+    bindkey "$complete_shortcut" qfc_complete
 
     function qfc_quick_command(){
       if [[ ! -z $1 ]] && [[ ! -z $2 ]] && [[ ! -z $3 ]]; then
         func_name='quick_'$1
         eval $"function $func_name(){
-          zle kill-whole-line 
+          zle kill-whole-line
           qfc_complete
           if [[ ! -z \${BUFFER} ]]; then
             c='$3'
@@ -74,8 +74,8 @@ if [[ -n "$ZSH_VERSION" ]]; then
             zle accept-line
           fi
         }"
-        zle -N $func_name 
-        bindkey "$2" $func_name 
+        zle -N $func_name
+        bindkey "$2" $func_name
       fi
     }
 
@@ -85,7 +85,7 @@ elif [[ -n "$BASH" ]]; then
         # pretty similar to zsh flow
         offset=${READLINE_POINT}
         READLINE_POINT=0
-        col=$(echo $(get_cursor_position) | cut -f 2 -d " ")
+        col=$(get_cursor_position | cut -f 2 -d " ")
 
         word=${READLINE_LINE:0:offset}
         word=${word##* }
@@ -100,7 +100,7 @@ elif [[ -n "$BASH" ]]; then
         READLINE_LINE=${READLINE_LINE:0:$((offset-word_length))}${result}${READLINE_LINE:$((offset))}
         offset=$(($offset - $word_length + $result_length))
 
-        row=$(echo $(get_cursor_position) | cut -f 1 -d " ")
+        row=$(get_cursor_position | cut -f 1 -d " ")
         tput cup $row $col
         READLINE_POINT=${offset}
     }
